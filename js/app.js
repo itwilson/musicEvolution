336f5bf38d72d90788fe73dc0838cfa964ad47cf
itwilson <itwilson@lawn-128-61-25-45.lawn.gatech.edu>
2014-10-30 16:32:22 -0400

    1.02
    -Added the word clouds. There is now groundwork laid for all of our
    vis’s. YEA!

--- js/app.js
+++ js/app.js
@@ -178,103 +178,163 @@
             
             
             
-            /*
-            var sortData = _.countBy(dataArray, function(num) {
-                for (i = 1; i < 11;) {
-                    if (num < i) {
-                        return i - 1;
-                        break;
-                    };
-                    i += .2;
-                };
-                
-              
-            });
-            
-            var sortData2 = _.countBy(dataArray2, function(num) {
-                for (i = 1; i < 11;) {
-                    if (num < i) {
-                        return i - 1;
-                        break;
-                    };
-                    i += .2;
-                };
-                
-              
-            });
-            
-            var sortData3 = _.countBy(dataArray3, function(num) {
-                for (i = 1; i < 11;) {
-                    if (num < i) {
-                        return i - 1;
-                        break;
-                    };
-                    i += .2;
-                };
-                
-              
-            });
-            
-            console.log(sortData);
-            var sortedValues = _.values(sortData);
-            var sortedKeys = _.keys(sortData);
-            
-    
-            canvas = d3.select('#chart').append("svg").attr("width", 500).attr("height", 200);
-            
-            canvas.selectAll('bars').data(sortedValues)
-                    .enter()
-                    .append('rect')
-                    .attr("x", function(d, i) { return 7 * i;})
-                    .attr("y", function(d) { return 200 - (d * 10);})
-                    .attr("height", function(d) { return d * 15;})
-                    .attr("width", 5)
-                    .style('fill', 'steelblue');
-                    
-            var sortedValues2 = _.values(sortData2);
-            var sortedKeys2 = _.keys(sortData2);
-            
-    
-            canvas2 = d3.select('#chart').append("svg").attr("width", 500).attr("height", 200).style("display", "inline-block");
-            
-            canvas2.selectAll('bars').data(sortedValues2)
-                    .enter()
-                    .append('rect')
-                    .attr("x", function(d, i) { return 7 * i;})
-                    .attr("y", function(d) { return 0;})
-                    .attr("height", function(d) { return d * 15;})
-                    .attr("width", 5)
-                    .style('fill', 'darkgreen');
-            
-            
-
-            var sortedValues3 = _.values(sortData3);
-            var sortedKeys3 = _.keys(sortData3);
-            
-    
-            canvas3 = d3.select('#chart').append("svg").attr("width", 500).attr("height", 200).style("display", "inline-block");
-            
-            canvas3.selectAll('bars').data(sortedValues3)
-                    .enter()
-                    .append('rect')
-                    .attr("x", function(d, i) { return 7 * i;})
-                    .attr("y", function(d) { return 0;})
-                    .attr("height", function(d) { return d * 15;})
-                    .attr("width", 5)
-                    .style('fill', 'darkgreen');
-            */
             
         });
 	});
 
 	infovisApp.controller('locationController', function($scope) {
 		$scope.message = 'On the Locations page';
+        
+        var width = 960,
+            height = 500;
+
+        var projection = d3.geo.mercator()
+            .center([0, 5 ])
+            .scale(150)
+            .rotate([-180,0]);
+
+        var svg = d3.select("#map").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var path = d3.geo.path()
+            .projection(projection);
+
+        var g = svg.append("g");
+
+        // load and display the World
+        d3.json("world-110m2.json", function(error, topology) {
+            g.selectAll("path")
+              .data(topojson.object(topology, topology.objects.countries)
+                  .geometries)
+            .enter()
+              .append("path")
+              .attr("d", path)
+        });
 	});
 
     infovisApp.controller('lyricsController', function($scope) {
 		$scope.message = 'On the Lyrics page';
+        
+            var fill = d3.scale.category20();
+          d3.layout.cloud().size([300, 300])
+              .words([
+                "Hello", "world", "normally", "you", "want", "more", "words",
+                "than", "this"].map(function(d) {
+                return {text: d, size: 10 + Math.random() * 90};
+              }))
+              .padding(5)
+              .rotate(function() { return ~~(Math.random() * 2) * 90; })
+              .font("Impact")
+              .fontSize(function(d) { return d.size; })
+              .on("end", draw)
+              .start();
+        
+          function draw(words) {
+            d3.select("#genre1cloud").append("svg")
+                .attr("width", 300)
+                .attr("height", 300)
+              .append("g")
+                .attr("transform", "translate(150,150)")
+              .selectAll("text")
+                .data(words)
+              .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("font-family", "Impact")
+                .style("fill", function(d, i) { return fill(i); })
+                .attr("text-anchor", "middle")
+                .attr("transform", function(d) {
+                  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+          };
+        
+    
+          d3.layout.cloud().size([300, 300])
+              .words([
+                "Hello", "world", "normally", "you", "want", "more", "words",
+                "than", "this"].map(function(d) {
+                return {text: d, size: 10 + Math.random() * 90};
+              }))
+              .padding(5)
+              .rotate(function() { return ~~(Math.random() * 2) * 90; })
+              .font("Impact")
+              .fontSize(function(d) { return d.size; })
+              .on("end", draw2)
+              .start();
+        
+          function draw2(words) {
+            d3.select("#genre2cloud").append("svg")
+                .attr("width", 300)
+                .attr("height", 300)
+              .append("g")
+                .attr("transform", "translate(150,150)")
+              .selectAll("text")
+                .data(words)
+              .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("font-family", "Impact")
+                .style("fill", function(d, i) { return fill(i); })
+                .attr("text-anchor", "middle")
+                .attr("transform", function(d) {
+                  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+          };
+        
+    
+        
+        
+        
 	});
 
     infovisApp.controller('subgenresController', function($scope) {
 		$scope.message = 'On the SubGenres page';
+        var margin = {top: 40, right: 10, bottom: 10, left: 10},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var color = d3.scale.category20c();
+
+        var treemap = d3.layout.treemap()
+            .size([width, height])
+            .sticky(true)
+            .value(function(d) { return d.size; });
+
+        var div = d3.select("#treemap").append("div")
+            .style("position", "relative")
+            .style("width", (width + margin.left + margin.right) + "px")
+            .style("height", (height + margin.top + margin.bottom) + "px")
+            .style("left", margin.left + "px")
+            .style("top", margin.top + "px");
+
+        d3.json("tree.json", function(error, root) {
+          var node = div.datum(root).selectAll(".node")
+              .data(treemap.nodes)
+            .enter().append("div")
+              .attr("class", "node")
+              .call(position)
+              .style("background", function(d) { return d.children ? color(d.name) : null; })
+              .text(function(d) { return d.children ? null : d.name; });
+
+          d3.selectAll("input").on("change", function change() {
+            var value = this.value === "count"
+                ? function() { return 1; }
+                : function(d) { return d.size; };
+
+            node
+                .data(treemap.value(value).nodes)
+              .transition()
+                .duration(1500)
+                .call(position);
+          });
+        });
+
+        function position() {
+          this.style("left", function(d) { return d.x + "px"; })
+              .style("top", function(d) { return d.y + "px"; })
+              .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+              .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+        }
+
 	});
